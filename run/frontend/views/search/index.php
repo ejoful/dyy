a182f5fa92609bf6dcd769c417a849596b4a8658
zhaoruikkk <929199940@qq.com>
2018-11-11 16:54:14 +0800

    modified:   run/frontend/views/search/index.php

--- run/frontend/views/search/index.php
+++ run/frontend/views/search/index.php
@@ -6,69 +6,133 @@ $this->title = 'BT电影天堂-迅雷BT种子下载|高清电影下载';
 ?>
 <div class="list_so">
   <h1>关键词“
-    <font color="red">黄日华</font>”的搜索结果</h1>
+    <font color="red">哈利波特</font>”的搜索结果</h1>
   <dl>
     <dt class="lf">
-      <a class="so_pic" href="/btdy/dy11579.html" title="追龙" target="_blank">
-        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201710/1d1c9cec07e06924.jpg?h=190" alt="追龙" src="http://gif-china.cc/uploads/allimg/201710/1d1c9cec07e06924.jpg?h=190" style="display: inline;">
-        <span>抢先版</span></a>
-    </dt>
-    <dd class="lf">
-      <p>名称：
-        <strong>
-          <a href="/btdy/dy11579.html" title="追龙" target="_blank">《追龙》</a></strong>
-        <span>电影</span></p>
-      <p>上映时间：2017 / 地区：香港 / 评分：7.5</p>
-      <p>主演：甄子丹，刘德华，姜皓文，郑则仕，刘浩龙，汤镇业，喻亢，胡然，徐冬冬，黄日华，陈惠敏，周丽淇，吴毅将，</p>
-      <p>简介：电影讲述了香港现代史上两大著名狠角色大毒枭跛豪（甄子丹饰）、五亿探长雷洛（刘德华饰）的传奇故事。 上世纪六七十年代，香港由英国殖民，权势腐败、社会混乱。1963年，穷困潦倒的青年阿豪（甄子丹饰）偷渡至香港，抱着“生死有命、富贵在天”之心决意一搏人生。阿豪带着几个兄弟，从九龙城寨底层开始一路刀刃舔血，爬上香港毒品霸主之位，一手掌控香港十大黑帮，江湖人称“跛豪”。而雷洛（刘德华饰）则从一位普通探长一步步爬上华人总探长高位，统领全香港三万警..</p>
-    </dd>
-  </dl>
-  <dl>
-    <dt class="lf">
-      <a class="so_pic" href="/btdy/dy10922.html" title="拆弹专家" target="_blank">
-        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201706/d0f5df06ff76a178.jpg?h=190" alt="拆弹专家" src="http://gif-china.cc/uploads/allimg/201706/d0f5df06ff76a178.jpg?h=190" style="display: inline;">
+      <a class="so_pic" href="/btdy/dy2844.html" title="哈利·波特与死亡圣器(下)" target="_blank">
+        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201604/2e1555ad3cf3eea9.jpg?h=190" alt="哈利·波特与死亡圣器(下)" src="http://gif-china.cc/uploads/allimg/201604/2e1555ad3cf3eea9.jpg?h=190" style="display: inline;">
         <span>高清</span></a>
     </dt>
     <dd class="lf">
       <p>名称：
         <strong>
-          <a href="/btdy/dy10922.html" title="拆弹专家" target="_blank">《拆弹专家》</a></strong>
-        <span>电影</span></p>
-      <p>上映时间：2017 / 地区：香港 / 评分：6.7</p>
-      <p>主演：刘德华，姜武，宋佳，吴卓羲，姜皓文，王紫逸，黄日华，石修，廖启智，张竣杰，蔡瀚亿，张继聪，李国麟，骆应钧，尹扬明，卢惠..</p>
-      <p>简介：章在山（刘德华 饰）是香港警队“爆炸品处理科”的一名高级督察。七年前，他潜伏到头号通缉犯火爆（姜武 饰）的犯罪团伙中，在一次打劫金库的行动中，章在山表露了其拆弹组卧底的身份，与警方里应外合，成功阻止炸弹引爆，并将火爆及其弟的犯罪组织一网打尽，可惜在千钧一发之间，火爆逃脱并扬言誓要报仇。复职后的章在山很快被晋升为警队的拆弹专家。七年后，香港接二连三遭遇炸弹恐怖袭击，警方更收到线报大批爆炸品已偷运入港，一切迹象显示香港将有大案发生。就在香..</p>
+          <a href="/btdy/dy2844.html" title="哈利·波特与死亡圣器(下)" target="_blank">《哈利·波特与死亡圣器(下)》</a></strong>
+        <span>电影 蓝光原盘 3D电影</span></p>
+      <p>上映时间：2011 / 地区：欧美 / 评分：8.0</p>
+      <p>主演：丹尼尔·雷德克里夫,艾玛·沃森,鲁伯特·格林特,海伦娜·伯翰·卡特,拉尔夫·费因斯,艾伦·瑞克曼,玛吉·史密斯,汤姆·..</p>
+      <p>简介：当又一次和伏地魔（拉尔夫·费因斯 Ralph Fiennes 饰）的意识连通，哈利·波特（丹尼尔·雷德克里夫 Daniel Radcliffe 饰）断定最后一件魂器藏在霍格沃茨，于是和罗恩（鲁伯特·格林特 Rupert Grint 饰）、赫敏（艾玛·沃森 Emma Watson 饰）一同返回阴云密布的学校。在好友们的帮助下，他们成功驱逐了斯内普（艾伦·瑞克曼 Alan Rickman 饰），然而觉察到哈利目的的伏地魔则率领徒众向霍格沃..</p>
     </dd>
   </dl>
   <dl>
     <dt class="lf">
-      <a class="so_pic" href="/btdy/dy5500.html" title="打开我天空" target="_blank">
-        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201607/d812bf446b54c7b7.jpg?h=190" alt="打开我天空" src="http://gif-china.cc/uploads/allimg/201607/d812bf446b54c7b7.jpg?h=190" style="display: inline;">
+      <a class="so_pic" href="/btdy/dy2843.html" title="哈利·波特与死亡圣器(上)" target="_blank">
+        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201604/37f049957fecdad0.jpg?h=190" alt="哈利·波特与死亡圣器(上)" src="http://gif-china.cc/uploads/allimg/201604/37f049957fecdad0.jpg?h=190" style="display: inline;">
         <span>高清</span></a>
     </dt>
     <dd class="lf">
       <p>名称：
         <strong>
-          <a href="/btdy/dy5500.html" title="打开我天空" target="_blank">《打开我天空》</a></strong>
-        <span>电影</span></p>
-      <p>上映时间：2016 / 地区：香港 / 评分：5.0</p>
-      <p>主演：曾志伟，方皓玟，陈冠希，王敏奕，恬妞，徐天佑，黄日华</p>
-      <p>简介：影片由四个单元故事构成，虽然每一个故事都是各自发生在不同的角落，但它们都是环绕著追寻梦想的理念。郑思杰执导《等》，讲述一名出册监犯每天坐在一间茶餐厅等待昔日女友前跟他言好，以往浪费青春，他应该从何重新起步？麦曦茵导演《青春》，两位年过半百的退休男女，能否衝破年龄界线开展一段黄金花之恋? 唐家伟执导之《盗亦有导》，讲述大盗叶欢受到阿细的电影热情感染，两人策划一齣打劫银行的警匪电影，决定来一场真人秀「大龙凤」。</p>
+          <a href="/btdy/dy2843.html" title="哈利·波特与死亡圣器(上)" target="_blank">《哈利·波特与死亡圣器(上)》</a></strong>
+        <span>电影 蓝光原盘 3D电影</span></p>
+      <p>上映时间：2010 / 地区：欧美 / 评分：8.0</p>
+      <p>主演：丹尼尔·雷德克里夫,艾玛·沃森,鲁伯特·格林特,海伦娜·伯翰·卡特,拉尔夫·费因斯,比尔·奈伊,约翰·赫特,艾伦·瑞克..</p>
+      <p>简介：邓不利多死后，伏地魔（Ralph Fiennes 饰）与食死徒入侵魔法学校，魔法部也被伏地魔的爪牙操控，邪恶的阴云笼罩魔法世界上空。在哈利·波特（丹尼尔·雷德克里夫 Daniel Radcliffe 饰）17岁生日之际，凤凰社成员及一众好友护送他回到了凤凰社的据点陋居，然而这立即遭到食死徒毁灭性地打击。哈利和罗恩（鲁伯特·格林特 Rupert Grint 饰）、赫敏（爱玛·沃特森 Emma Watson 饰）侥幸逃亡，并且按照邓不利多..</p>
     </dd>
   </dl>
   <dl>
     <dt class="lf">
-      <a class="so_pic" href="/btdy/dy3297.html" title="光辉岁月" target="_blank">
-        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201605/4d72c33ba39cd4d5.jpg?h=190" alt="光辉岁月" src="http://gif-china.cc/uploads/allimg/201605/4d72c33ba39cd4d5.jpg?h=190" style="display: inline;">
+      <a class="so_pic" href="/btdy/dy2842.html" title="哈利·波特与混血王子" target="_blank">
+        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201604/1111b5fad8d0428a.jpg?h=190" alt="哈利·波特与混血王子" src="http://gif-china.cc/uploads/allimg/201604/1111b5fad8d0428a.jpg?h=190" style="display: inline;">
         <span>高清</span></a>
     </dt>
     <dd class="lf">
       <p>名称：
         <strong>
-          <a href="/btdy/dy3297.html" title="光辉岁月" target="_blank">《光辉岁月》</a></strong>
+          <a href="/btdy/dy2842.html" title="哈利·波特与混血王子" target="_blank">《哈利·波特与混血王子》</a></strong>
         <span>电影 蓝光原盘</span></p>
-      <p>上映时间：2013 / 地区：大陆 / 评分：4.0</p>
-      <p>主演：曾志伟,黄日华,梁咏琪,郭涛,任达华,惠英红,刘五性,吕良伟,狄龙,倪虹洁,莫少聪,熊欣欣,王敏德,李炜</p>
-      <p>简介：清朝末年，局势动荡，乱相纷现。曾经历黄花岗起义的南国革命党人铁云（黄日华 饰）和志同道合的战友勇闯大漠，偷运黄金，准备购买大批军火再次起事。谁知途中遭到博尔格亲王（吕良伟 饰）之爪牙满天红（倪虹洁 饰）的伏击，关键时刻幸得满腔赤诚的从四品官卓知府（狄龙 饰）出手相救。卓知府具有远见卓识，虽身处敌我两派，但仍将国家的命运寄托在铁云身上，安排他前往黄金峡寻找善良热情且不乏豪气的猫老板（曾志伟 饰），为此甘愿陪上满门家小性命。猫老板是曾参加..</p>
+      <p>上映时间：2009 / 地区：欧美 / 评分：7.0</p>
+      <p>主演：丹尼尔·雷德克里夫,艾玛·沃森,鲁伯特·格林特,汤姆·费尔顿,迈克尔·刚本,艾伦·瑞克曼,邦妮·怀特,朱丽·沃特斯,海..</p>
+      <p>简介：这一集中，食死徒开始肆无忌惮横行于麻瓜和魔法两个世界中，霍格沃茨内忧外患，危机重重。邓不利多（迈克尔·甘本 Michael Gambon 饰）带着哈利（丹尼尔·雷德克里夫 Daniel Radcliffe 饰）辗转找到隐居多年的鲁弗斯·斯克林杰，请他出山接替斯内普（艾伦·里克曼 Alan Rickman 饰）担任魔药学的老师。鲁弗斯曾经教授过无数优秀的学生，而汤姆·鲁斯——伏地魔——正是他最出色的一名弟子。邓不利多深信鲁弗斯保有伏地魔..</p>
+    </dd>
+  </dl>
+  <dl>
+    <dt class="lf">
+      <a class="so_pic" href="/btdy/dy2841.html" title="哈利·波特与凤凰社" target="_blank">
+        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201604/736992f7fa0700b4.jpg?h=190" alt="哈利·波特与凤凰社" src="http://gif-china.cc/uploads/allimg/201604/736992f7fa0700b4.jpg?h=190" style="display: inline;">
+        <span>高清</span></a>
+    </dt>
+    <dd class="lf">
+      <p>名称：
+        <strong>
+          <a href="/btdy/dy2841.html" title="哈利·波特与凤凰社" target="_blank">《哈利·波特与凤凰社》</a></strong>
+        <span>电影 蓝光原盘</span></p>
+      <p>上映时间：2007 / 地区：欧美 / 评分：7.0</p>
+      <p>主演：丹尼尔·雷德克里夫,艾玛·沃森,鲁伯特·格林特,加里·奥德曼,拉尔夫·费因斯,玛吉·史密斯,迈克尔·刚本,艾伦·瑞克曼..</p>
+      <p>简介：逃过摄魂怪的追杀，通过魔法部的审查，哈利（丹尼尔·雷德克里夫）终于回到美丽的霍格沃兹。然而这个学期并不轻松，他不仅要完成魔法等级考试，还要应对夜晚的噩梦和黑魔法防御课老师乌姆里奇的打压。在好友的支持下，哈利秘密组建了邓不利多军团，与大家一起练习黑魔法防御术。&nbsp; 噩梦中，哈利总是以伏地魔的视角出现，似乎他们之间有着某种神秘的联系。在邓不利多（迈克尔·刚本）的指示下，哈利向斯内普教授（阿伦·瑞克曼）学习封闭术，却鲜有成效。终于在..</p>
+    </dd>
+  </dl>
+  <dl>
+    <dt class="lf">
+      <a class="so_pic" href="/btdy/dy2840.html" title="哈利·波特与火焰杯" target="_blank">
+        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201604/b1f40184bd287eea.jpg?h=190" alt="哈利·波特与火焰杯" src="http://gif-china.cc/uploads/allimg/201604/b1f40184bd287eea.jpg?h=190" style="display: inline;">
+        <span>高清</span></a>
+    </dt>
+    <dd class="lf">
+      <p>名称：
+        <strong>
+          <a href="/btdy/dy2840.html" title="哈利·波特与火焰杯" target="_blank">《哈利·波特与火焰杯》</a></strong>
+        <span>电影 蓝光原盘</span></p>
+      <p>上映时间：2005 / 地区：欧美 / 评分：8.0</p>
+      <p>主演：丹尼尔·雷德克里夫,艾玛·沃森,鲁伯特·格林特,蒂莫西·斯波,邦妮·怀特,罗伯特·帕丁森,汤姆·费尔顿,梁佩诗,迈克尔..</p>
+      <p>简介：这是一个不同寻常的学期，魔法学校的盛事：三强争霸赛在霍格沃茨举行。学生们争先报名，希翼获得永恒无上的荣誉。火焰杯在选出三所学校的三名勇士后，竟将未足年龄的哈利也挑选出来。&nbsp; 哈利（Daniel Radcliffe饰）的当选引起其他学生的嫉妒连连，就连铁哥们罗恩（Rupert Grint饰）也首次和他闹起了别扭，哈利感到孤立无援，还好有教父小天狼星和赫敏一直支持他。可总有些好事者如记者丽塔等着看他的好戏。天知道这场比赛是何等艰..</p>
+    </dd>
+  </dl>
+  <dl>
+    <dt class="lf">
+      <a class="so_pic" href="/btdy/dy2839.html" title="哈利·波特与阿兹卡班的囚徒" target="_blank">
+        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201604/ebfa15a19c03e734.jpg?h=190" alt="哈利·波特与阿兹卡班的囚徒" src="http://gif-china.cc/uploads/allimg/201604/ebfa15a19c03e734.jpg?h=190" style="display: inline;">
+        <span>高清</span></a>
+    </dt>
+    <dd class="lf">
+      <p>名称：
+        <strong>
+          <a href="/btdy/dy2839.html" title="哈利·波特与阿兹卡班的囚徒" target="_blank">《哈利·波特与阿兹卡班的囚徒》</a></strong>
+        <span>电影 蓝光原盘</span></p>
+      <p>上映时间：2004 / 地区：欧美 / 评分：8.0</p>
+      <p>主演：丹尼尔·雷德克里夫,艾玛·沃森,鲁伯特·格林特,加里·奥德曼,朱丽·沃特斯,邦妮·怀特,大卫·休里斯,迈克尔·刚本,艾..</p>
+      <p>简介：哈利（丹尼尔·雷德克里夫）即将在霍格沃兹渡过第三个年头，此时在阿兹塔班却传出恶棍小天狼星（加里·奥德曼）越狱的消息。据说小天狼星正是背叛哈利父母的好友，他的教父，而这次小天狼星越狱似乎正是为了找他。哈利的心里悄悄的滋生了为父母报仇的想法，期待着小天狼星的出现。&nbsp; 新来的魔法老师卢平（大卫·休里斯）有着桀骜不驯的个性，与哈利关系很好，教了哈利许多实用的黑魔法防御知识。而此时在他的魔法地图上却出现了一个神秘人物，虫尾巴。偶然的机..</p>
+    </dd>
+  </dl>
+  <dl>
+    <dt class="lf">
+      <a class="so_pic" href="/btdy/dy2838.html" title="哈利·波特与魔法石" target="_blank">
+        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201604/9d89392b7cc25479.jpg?h=190" alt="哈利·波特与魔法石" src="http://gif-china.cc/uploads/allimg/201604/9d89392b7cc25479.jpg?h=190" style="display: inline;">
+        <span>高清</span></a>
+    </dt>
+    <dd class="lf">
+      <p>名称：
+        <strong>
+          <a href="/btdy/dy2838.html" title="哈利·波特与魔法石" target="_blank">《哈利·波特与魔法石》</a></strong>
+        <span>电影 蓝光原盘</span></p>
+      <p>上映时间：2001 / 地区：欧美 / 评分：8.0</p>
+      <p>主演：丹尼尔·雷德克里夫,艾玛·沃森,鲁伯特·格林特,艾伦·瑞克曼,玛吉·史密斯,汤姆·费尔顿,伊恩·哈特,理查德·哈里斯,..</p>
+      <p>简介：哈利波特是一个孤儿，从小寄养在姨妈家，受尽欺凌。但就在哈利11岁生日的时候，他意外收到了霍格沃茨学院的入学通知书。哈利从该学院派来接他的巨人海格口中得知，这是一间魔法学院，并得知了自己的身世，原来哈利的父母都是伟大的魔法师，在对付伏地魔的战斗中双双献身，唯有哈利幸免于难。&nbsp; 哈利进入霍格沃茨后，表现出了超乎想象的飞行天赋，得到麦格教授的推荐进入了格兰芬多的魁地奇球队。另一方面，哈利发现霍格沃茨学院内有一股黑暗势力似乎在暗暗滋..</p>
+    </dd>
+  </dl>
+  <dl>
+    <dt class="lf">
+      <a class="so_pic" href="/btdy/dy1679.html" title="哈利·波特与密室" target="_blank">
+        <img class="lazy" data-src="http://gif-china.cc/uploads/allimg/201601/e7d730b6a6cc1786.jpg?h=190" alt="哈利·波特与密室" src="http://gif-china.cc/uploads/allimg/201601/e7d730b6a6cc1786.jpg?h=190" style="display: inline;">
+        <span>高清</span></a>
+    </dt>
+    <dd class="lf">
+      <p>名称：
+        <strong>
+          <a href="/btdy/dy1679.html" title="哈利·波特与密室" target="_blank">《哈利·波特与密室》</a></strong>
+        <span>电影 蓝光原盘</span></p>
+      <p>上映时间：2002 / 地区：欧美 / 评分：8.0</p>
+      <p>主演：丹尼尔·雷德克利弗，艾玛·沃特森，鲁伯特·格林特，肯尼斯·布拉纳夫，阿伦·里克曼，约翰·克利斯</p>
+      <p>简介：哈利波特（丹尼尔•雷德克里夫 Daniel Radcliffe 饰）结束了假期，即将回到霍格沃兹继续学习魔法。一个叫多比的家养小精灵警告哈利不要回到霍格沃兹，否则会陷入极大的危险。哈利没有听从多比的劝告，回到了霍格沃兹。很快，霍格沃兹发生了一连窜怪事:接二连三出现学生被石化，一直找不出原因。而哈利总能听到一种奇怪的声音，从墙壁里传出来。&nbsp; 传说，霍格沃兹有一个密室，里面记录着伏地魔年轻时的秘密，只有斯莱特林的人才能打开密室。..</p>
     </dd>
   </dl>
 </div>
